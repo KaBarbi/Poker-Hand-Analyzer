a934662bc7a75bfa909a5e02e65fa2b6d3626a97
KaBarbi <135548335+KaBarbi@users.noreply.github.com>
2026-01-02 14:36:20 -0300

    first commit

---
+++ cards.py
@@ -0,0 +1,6 @@
+# 52 Cards
+
+# value: 2-14 (where 14 = Ace)
+# suit: 'H', 'D', 'C', 'S'
+
+# ('A', 'S') → (14, 'S')
---
+++ hand_ev.py
@@ -0,0 +1,18 @@
+# list of 7 cards (2 from hand + up to 5 community cards)
+# example
+(8, [14])        # straight flush A-high
+(6, [13, 10])    # full house K over 10
+
+# Rankings:
+'''
+0 High Card
+1 One Pair
+2 Two Pair
+3 Three of a Kind
+4 Straight
+5 Flush
+6 Full House
+7 Four of a Kind
+8 Straight Flush
+9 Royal Flush
+'''
