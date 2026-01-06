6e813855481082a0c0853ffda97c32fe73edd568
KaBarbi <135548335+KaBarbi@users.noreply.github.com>
2026-01-06 13:31:55 -0300

    [feat]: test for tripes

--- tests.py
+++ tests.py
@@ -1,5 +1,5 @@
 from cards import create_deck, card_to_value, remove_known_cards
-from hand_ev import evaluate_hand, ONE_PAIR, HIGH_CARD, TWO_PAIR
+from hand_ev import evaluate_hand, ONE_PAIR, HIGH_CARD, TWO_PAIR, THREE_KIND
 
 if __name__ == "__main__":
     deck = create_deck()
@@ -43,3 +43,15 @@ cards = [
 rank, tb = evaluate_hand(cards)
 assert rank == TWO_PAIR
 assert tb == [14, 10, 7]
+
+
+cards = [
+    (12, 0), (12, 1), (12, 2),  # QQQ
+    (14, 0), (10, 1), (5, 2), (2, 3)
+]
+
+rank, tb = evaluate_hand(cards)
+assert rank == THREE_KIND
+assert tb == [12, 14, 10]
+
+print("THREE OF A KIND OK")
